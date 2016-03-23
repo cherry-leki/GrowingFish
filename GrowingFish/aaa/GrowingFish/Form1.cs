f8ca2c38721c07b101e789181a716dc16f8bdd22
Leki <Leki>
2016-03-23 21:12:32 +0900

    create MainCharacter class

---
+++ GrowingFish/aaa/GrowingFish/Form1.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace GrowingFish
+{
+    public partial class Form1 : Form
+    {
+        MainCharacter mainCharacter;
+        Graphics g;
+        int counter;
+
+        public Form1()
+        {
+            InitializeComponent();
+            mainCharacter = new MainCharacter();
+            g = this.CreateGraphics();
+            
+            this.Refresh();
+
+            Thread th = new Thread(new ThreadStart(run));
+            th.Start();
+        }
+
+        public void run()
+        {
+            while (true)
+            {
+                mainCharacter.keyProcess(counter);
+                Refresh();
+                try
+                {
+                    Thread.Sleep(5);
+                }
+                catch (ThreadInterruptedException e)
+                {
+                }
+                counter++;
+            }
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            Pen pen = new Pen(Color.Black, 5);
+            Rectangle rec = new Rectangle((int)mainCharacter.x, (int)mainCharacter.y, 20, 20);
+            g.DrawArc(pen, rec, 0 ,360);
+            //g.DrawRectangle(pen, rec);
+        }
+
+        private void keyPressed(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.Up:
+                    {
+                        mainCharacter.keyUp = true;
+                        if (mainCharacter.fishSpeed < 40)
+                        {
+                            mainCharacter.fishSpeed++;
+                        }   
+                    }break;
+                case Keys.Down:
+                    {
+                        mainCharacter.keyDown = true;
+                        if (mainCharacter.fishSpeed < 40)
+                        {
+                            mainCharacter.fishSpeed++;
+                        }
+                    }break;
+                case Keys.Left:
+                    {
+                        mainCharacter.keyLeft = true;
+                        if (mainCharacter.fishSpeed < 40)
+                        {
+                            mainCharacter.fishSpeed++;
+                        }
+                    }break;
+                case Keys.Right:
+                    {
+                        mainCharacter.keyRight = true;
+                        if (mainCharacter.fishSpeed < 40)
+                        {
+                            mainCharacter.fishSpeed++;
+                        }
+                    }break;
+            }
+        }
+        public void keyReleased(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.Up:
+                    {
+                        mainCharacter.keyUp = false;
+                        mainCharacter.finalKey = "up";
+                    }break;
+                case Keys.Down:
+                    {
+                        mainCharacter.keyDown = false;
+                        mainCharacter.finalKey = "down";
+                    } break;
+                case Keys.Left:
+                    {
+                        mainCharacter.keyLeft = false;
+                        mainCharacter.finalKey2 = "left";
+                    } break;
+                case Keys.Right:
+                    {
+                        mainCharacter.keyRight = false;
+                        mainCharacter.finalKey2 = "right";
+                    } break;
+            }
+        }
+    }
+}
---
+++ GrowingFish/aaa/GrowingFish/MainCharacter.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+
+
+namespace GrowingFish
+{
+    public class MainCharacter
+    {
+        public string finalKey, finalKey2;
+        public double x, y;
+        public bool keyUp, keyDown, keyLeft, keyRight;
+        public double fishSpeed;
+        public int fishStatus;
+        //public Bitmap[] fishImg;
+        public double fishWidth, fishHeight;
+
+        public MainCharacter()
+        {
+            x = 500;
+            y = 384;
+            fishSpeed = 3;
+            //fishImg = new Bitmap[5];
+
+            //for (int i = 1; i < fishImg.Length; i++)
+            //{
+                //fishImg[i] = new Bitmap("");
+            //}
+            //fishWidth = fishImg[1].Width;
+            //fishHeight = fishImg[1].Height;
+        }
+        public void keyProcess(int counter)
+        {
+            if (keyUp == true)
+            {
+                if (y > 40)
+                {
+                    y -= (fishSpeed) / 15;
+                    if (fishStatus == 1 || fishStatus == 3)
+                    {
+                        if ((counter / 8 % 2) == 0)
+                        {
+                            fishStatus = 1;
+                        }
+                        else
+                        {
+                            fishStatus = 3;
+                        }
+                    }
+                }
+                else
+                {
+                    if ((counter / 8 % 2) == 0)
+                    {
+                        fishStatus = 0;
+                    }
+                    else
+                    {
+                        fishStatus = 2;
+                    }
+                }
+            }
+
+            if (keyDown == true)
+            {
+                if (y < 740)
+                {
+                    y += (fishSpeed) / 15;
+                    if (fishStatus == 1 || fishStatus == 3)
+                    {
+                        if ((counter / 8 % 2) == 0)
+                        {
+                            fishStatus = 1;
+                        }
+                        else
+                        {
+                            fishStatus = 3;
+                        }
+                    }
+                }
+                else
+                {
+                    if ((counter / 8 % 2) == 0)
+                    {
+                        fishStatus = 0;
+                    }
+                    else
+                    {
+                        fishStatus = 2;
+                    }
+                }
+            }
+
+            if (keyLeft == true)
+            {
+                if (x > 10)
+                    x -= (fishSpeed) / 15;
+                if ((counter / 8 % 2) == 0)
+                {
+                    fishStatus = 0;
+                }
+                else
+                {
+                    fishStatus = 2;
+                }
+            }
+
+            if (keyRight == true)
+            {
+                if (x < 980)
+                    x += (fishSpeed) / 15;
+                if ((counter / 8 % 2) == 0)
+                {
+                    fishStatus = 1;
+                }
+                else
+                {
+                    fishStatus = 3;
+                }
+            }
+            if (keyUp == false && keyDown == false && keyLeft == false && keyRight == false)
+            {
+                if (fishSpeed > 3)
+                {
+                    fishSpeed--;
+
+                    if (finalKey == "up" && y > 40) y -= (fishSpeed) / 10;
+                    if (finalKey == "down" && y < 740) y += (fishSpeed) / 10;
+                    if (finalKey == "left" && x > 10) x -= (fishSpeed) / 10;
+                    if (finalKey == "right" && x < 980) x += (fishSpeed) / 10;
+
+                    if (fishSpeed <= 10)
+                    {
+                        finalKey = "";
+                        finalKey2 = "";
+                    }
+                }
+            }
+        }
+    }
+}
